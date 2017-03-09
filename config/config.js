c24db53d5d7903d9c0f816dc6cd14e402e21ae59
Laura_EWE <lewe@adneom.com>
2017-03-09 15:21:28 +0100

    change the db credentials

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ var config = {
         },
         port: process.env.PORT || 8080,
         // db: 'mysql://root@localhost/wod_surveys'
-        db: 'mysql://wod:wodSurvey@localhost/wod'
+        db: 'postgres://hdwpzsvyqtiljc:9ae9516e094fc63e19191450088cfa2101332f2b6124d90a5f6a66b73e3845e3@ec2-54-221-244-196.compute-1.amazonaws.com/d55e6762k6ce3k'
             // root:password
     },
 
@@ -20,7 +20,7 @@ var config = {
             name: 'wod-survey'
         },
         port: process.env.PORT || 3000,
-        db: 'mysql://localhost/wod-survey-test'
+        db: 'postgres://hdwpzsvyqtiljc:9ae9516e094fc63e19191450088cfa2101332f2b6124d90a5f6a66b73e3845e3@ec2-54-221-244-196.compute-1.amazonaws.com/d55e6762k6ce3k'
     },
 
     production: {
@@ -29,7 +29,7 @@ var config = {
             name: 'wod-survey'
         },
         port: process.env.PORT || 3000,
-        db: 'mysql://localhost/wod-survey-production'
+        db: 'postgres://hdwpzsvyqtiljc:9ae9516e094fc63e19191450088cfa2101332f2b6124d90a5f6a66b73e3845e3@ec2-54-221-244-196.compute-1.amazonaws.com/d55e6762k6ce3k'
     }
 };
 
