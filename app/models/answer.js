bf31a9068410e056d7087f6ad4833931993b6e23
Laura_EWE <lewe@adneom.com>
2017-03-09 17:46:12 +0100

    add try and catch

--- app/models/answer.js
+++ app/models/answer.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
         classMethods: {
             associate: function(models) {
                 // Answer.belongsTo(models.Survey, { foreignKey: 'FK_survey', targetKey: 'id' }),
-                Answer.belongsTo(models.Question, { foreignKey: 'FK_question', targetKey: 'id' })
+                Answer.belongsTo(models.Question, { foreignKey: 'FK_question', targetKey: 'id' , constraints: false})
                 Answer.belongsTo(models.Survey, {foreignKey: 'FK_idSurvey', targetKey: 'idSurvey', constraints: false})
             }
         }
