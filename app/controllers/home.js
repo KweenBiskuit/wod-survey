659938122bf580679eef225a4a1977433fd283a1
Laura_EWE <lewe@adneom.com>
2017-03-09 10:29:16 +0100

    test route

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -36,6 +36,10 @@ router.post('/form', function (req, res, next) {
   res.status(200).json(req.body);
 });
 
+router.get('/test', function (req, res, next) {
+  res.sendStatus(200);
+});
+
 router.post('/ingest', upload.single('file'), function (req, res) {
   var filePath = req.file.path;
   var items = parser.readFile(filePath);
